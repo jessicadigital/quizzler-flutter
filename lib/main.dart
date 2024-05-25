0568e6399f948cb10417356d6d8a9d258ad46c44
Dr Jessica Muirhead <github@jessica.digital>
2024-05-25 20:16:45 +0100

    Fix dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -50,9 +50,11 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: TextButton(
+              style:ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll<Color>(Colors.green),
+                foregroundColor: WidgetStatePropertyAll<Color>(Colors.white),
+              ),
               child: Text(
                 'True',
                 style: TextStyle(
@@ -69,8 +71,10 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style:ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll<Color>(Colors.red),
+              ),
               child: Text(
                 'False',
                 style: TextStyle(
